e717026f5e720686a5535ee0592b6232ce40ccbb
Antifreeze <preston.patterson1@gmail.com>
2019-03-27 21:12:04 -0500

    updated DB with drinks

---
+++ db/schema2.sql
@@ -0,0 +1,1 @@
+SELECT * FROM cocktailDB.all_drinks_withid;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,2 @@
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+SELECT * FROM cocktailDB.all_drinks_withid;
+RENAME TABLE cocktailDB.all_drinks_withid TO cocktailDB.drinks;
