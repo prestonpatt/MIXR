d5f8cef6630de9bb5a88ce4256100e891c7b7baa
Antifreeze <preston.patterson1@gmail.com>
2019-03-28 06:42:00 -0500

    removed liqours table

--- db/schema.sql
+++ db/schema.sql
@@ -55,13 +55,3 @@ CREATE TABLE drinks (
     `strMeasure9` VARCHAR(18) CHARACTER SET utf8,
     `strVideo` INT
 );
-
-CREATE TABLE liquors
-(
-	id int NOT NULL AUTO_INCREMENT,
-	nameFirst varchar(255) NOT NULL,
-    nameLast varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    age integer(10) NOT NULL,
-	PRIMARY KEY (id)
-);
