9c98c6b36aacf3fb7cc451a3dc16eec602e0ce08
Antifreeze <preston.patterson1@gmail.com>
2019-03-30 17:19:06 -0500

    apiRoutes.js changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,25 +3,23 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    db.Drinks.findAll({}).then(function(dbExamples) {
+      res.status(200);
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+}
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+//   // Load example page and pass in an example by id
+//   app.get("/signin", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) 
+//     {
+//       res.json(dbExample)
+//     });
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// 
