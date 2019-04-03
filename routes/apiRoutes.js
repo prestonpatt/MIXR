fa0a6d8f51b3ca29e6843dbb9d234524b46aae76
Antifreeze <preston.patterson1@gmail.com>
2019-04-02 20:41:41 -0500

    updated routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,6 +5,7 @@ var db = require("../models");
 module.exports = function (app) {
   // Get all examples
   app.get("/api/drinks", function (req, res) {
+<<<<<<< Updated upstream
     db.drinks.findAll({}).then(function (result) {
       res.json(result);
     });
@@ -12,24 +13,47 @@ module.exports = function (app) {
   app.get("/api/:drink", function (req, res) {
     db.drinks.findAll({
       where: {
+=======
+    console.log(req.query);
+    var options = {};
+    if (req.query.attributes) {
+      options.attributes = req.query.attributes.split(",");
+    }
+    if (req.query.limit) {
+      options.limit = parseInt(req.query.limit);
+    }
+
+    if (req.query.find) {
+      options.where = {
+>>>>>>> Stashed changes
         $or: [
-          { 'strIngredient1': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient2': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient3': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient4': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient5': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient6': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient7': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient8': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient9': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient10': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient11': { like: '%' + req.params.drink + '%' } },
-          { 'strIngredient12': { like: '%' + req.params.drink + '%' } }
+          { 'strIngredient1': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient2': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient3': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient4': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient5': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient6': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient7': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient8': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient9': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient10': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient11': { like: '%' + req.query.find + '%' } },
+          { 'strIngredient12': { like: '%' + req.query.find + '%' } }
         ]
-      },
-    }).then(function (result) {
+
+      };
+    }
+
+    db.drinks.findAll(options).then(function (result) {
       res.json(result);
     });
-  // res.status(200)
+  }),
+
+
+  app.get("/api/drinks/:drinkId", function (req, res) {
+    db.drinks.findByPk(req.params.drinkId).then(function (result) {
+      res.json(result);
+    });
+    // res.status(200)
   });
 };
