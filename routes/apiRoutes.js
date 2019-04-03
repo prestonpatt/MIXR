42de7670b8fb80373e1041dfac2d2a4906c1270c
Antifreeze <preston.patterson1@gmail.com>
2019-04-02 23:01:27 -0500

    redone routes...again

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,6 +6,8 @@ module.exports = function (app) {
   // Get all examples
   app.get("/api/drinks", function (req, res) {
     var options = {};
+
+    
     if (req.query.attributes) {
       options.attributes = req.query.attributes.split(",");
     }
@@ -14,22 +16,9 @@ module.exports = function (app) {
     }
 
     if (req.query.find) {
-      options.where = {
-        $or: [
-          { 'strIngredient1': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient2': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient3': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient4': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient5': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient6': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient7': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient8': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient9': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient10': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient11': { like: '%' + req.query.find + '%' } },
-          { 'strIngredient12': { like: '%' + req.query.find + '%' } }
-        ]
-
+      options.include = {
+        model: db.ingredients,
+        where: {strName: { like: '%' + req.query.find + '%' }}
       };
     }
 
@@ -40,7 +29,12 @@ module.exports = function (app) {
 
 
   app.get("/api/drinks/:drinkId", function (req, res) {
-    db.drinks.findByPk(req.params.drinkId).then(function (result) {
+    db.drinks.findOne({
+      where: {
+        id: req.params.drinkId
+      },
+      include: [db.ingredients]
+    }).then(function (result) {
       res.json(result);
     });
     // res.status(200)
