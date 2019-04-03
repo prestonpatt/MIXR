3bc9166eaa54bb1cd7a659155c509c17b05808de
Antifreeze <preston.patterson1@gmail.com>
2019-04-03 17:11:43 -0500

    updated the ingredients model

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 module.exports = function (sequelize, DataTypes) {
   var Ingredients = sequelize.define("ingredients", {
-    strName: DataTypes.STRING,
     strIngredient: DataTypes.STRING,
     strMeasure: DataTypes.STRING
 
